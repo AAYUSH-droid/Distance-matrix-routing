a9a9a957485babde406c86eda59f37407681ff0d
AAYUSH SHARMA <aayushsharma@AAYUSHs-MacBook-Air.local>
2022-08-07 12:12:25 +0530

    added dragable marker functionality which when dragged changes latitude and longitude

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import "./App.css";
-
 import * as tt from "@tomtom-international/web-sdk-maps";
+import "@tomtom-international/web-sdk-maps/dist/maps.css";
 
 function App() {
   const mapElement = useRef();
@@ -24,34 +24,61 @@ function App() {
     });
 
     setMap(map);
+
+    const element = document.createElement("div");
+    element.className = "marker";
+
+    const addMarker = () => {
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+        .setLngLat([longitude, latitude])
+        .addTo(map);
+
+      marker.on("dragend", () => {
+        const lnglat = marker.getLngLat();
+        setLongitude(lnglat.lng);
+        setLatitude(lnglat.lat);
+        //lng lat on dragging updates longitude latitude
+        // console.log(lnglat);
+      });
+    };
+
+    addMarker();
+
     return () => map.remove();
   }, [longitude, latitude]);
   return (
-    <div className="app">
-      <div ref={mapElement} className="map">
-        <div className="search-bar">
-          <h1>Where to?</h1>
-          <input
-            type="text"
-            id="longitude"
-            className="longitude"
-            placeholder="Put in Longitude"
-            onChange={(e) => {
-              setLongitude(e.target.value);
-            }}
-          />
-          <input
-            type="text"
-            id="latitude"
-            className="latitude"
-            placeholder="Put in latitude"
-            onChange={(e) => {
-              setLatitude(e.target.value);
-            }}
-          />
+    <>
+      {map && (
+        <div className="app">
+          <div ref={mapElement} className="map">
+            <div className="search-bar">
+              <h1>Where to?</h1>
+              <input
+                type="text"
+                id="latitude"
+                className="latitude"
+                placeholder="Put in latitude"
+                onChange={(e) => {
+                  setLatitude(e.target.value);
+                }}
+              />
+              <input
+                type="text"
+                id="longitude"
+                className="longitude"
+                placeholder="Put in Longitude"
+                onChange={(e) => {
+                  setLongitude(e.target.value);
+                }}
+              />
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
 
